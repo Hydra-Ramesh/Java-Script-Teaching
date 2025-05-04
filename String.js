ba09f4bd67a5037fc6b8b5499f573d613d51f954
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:48:08 +0530

    Add Strings

---
+++ String.js
@@ -0,0 +1,17 @@
+let name = "Ramesh Das";//With Double Quote
+let address = 'Kultha';//With Single Quote
+let text = "Hello I a'm Ramesh Das";//Single Quote inside Double
+let myText = 'He said "Shut up"'//Double inside Single Quote according to my Use.
+
+
+let herText = `Hey Darling,
+Will You Marry me..?`//We can use it multiline string and for dinamic value;
+
+console.log(name, address);
+console.log(text);
+console.log(myText);
+console.log(herText);
+
+
+let PORT = 5000;
+console.log(`Your web page is Running at PORT number ${PORT}`);//Dynamic Printing
