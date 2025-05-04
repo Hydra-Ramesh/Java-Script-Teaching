58c42f935015a3b7266bd5aa05ee1991360d9c25
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:47:48 +0530

    Add Number Data Type with Type Of Operator

---
+++ Number.js
@@ -0,0 +1,7 @@
+let x = 721430 //Number 2^+53 to 2^-53
+let number = 8509778299117488n //Big Integer
+console.log(x);
+console.log(number);
+console.log(typeof(x)); // TypeOfOperator
+console.log(typeof(number));
+
