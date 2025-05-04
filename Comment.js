a5ab43374029c48b9201c316fa268a04af6c9012
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:03:09 +0530

    Add Comment Part

---
+++ Comment.js
@@ -0,0 +1,14 @@
+//Single Line Comment
+//Comment is a nesesary code line which is avoid by the compiler during the execution time.
+/**
+ * Multi Line comment
+ */
+
+ let x = 20;
+ console.log(x);
+
+/**
+ * 1) Simplify the code block
+ * 2) For describe the code for another developer.
+ * 3) Give TODO for feature update
+ */
