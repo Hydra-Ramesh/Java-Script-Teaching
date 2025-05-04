07c17c7149b9d5bf73c792f6cb258a6f6813d708
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:02:53 +0530

    Add the Variable

---
+++ Variable.js
@@ -0,0 +1,9 @@
+var x = 99;//We can Update.
+let y = 69;//We can Update.
+const z = 88;//We can't update due to type safty.
+x = 88;
+y = 99;
+z = 69;
+console.log(x);
+console.log(y);
+console.log(z);
