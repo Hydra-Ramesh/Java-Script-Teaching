4c45be8281cec4cc21d2e8fe909ceb6a166bdfdb
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:27:19 +0530

    Add Varibale Printing

---
+++ PlayWithVariable.js
@@ -0,0 +1,10 @@
+let studentName = "Sanju";
+var age = 19;
+
+console.log(studentName,age);
+
+console.log(studentName);
+console.log(age);
+
+console.log("My name is : ",studentName);
+console.log("My age : ", age);
