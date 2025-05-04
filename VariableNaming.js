b702b6faa7df124313e8166344aa7eb1756a3da7
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-04 22:27:01 +0530

    Add Naming Rule

---
+++ VariableNaming.js
@@ -0,0 +1,11 @@
+let myName = "Ramesh Das";//Camel Casing
+let age = 21;//Snake Casing
+let PORT = 3000; //Kabab Casing
+let HomeAddress = "Kultha"; //Pascal Casing
+
+/**
+ * 1) White Space are not allowed.
+ * 2) We can't use Number in start but can use in Middle.
+ * 3) Special Char and Keywords are not allowed.
+ * 4) We can use the _ in front.
+ */
