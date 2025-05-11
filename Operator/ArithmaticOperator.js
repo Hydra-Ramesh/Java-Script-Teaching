99d6a136006d7cd0d204ead8451b135d32aa9707
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-11 10:08:30 +0530

    Add the Arithmatic Operator

---
+++ Operator/ArithmaticOperator.js
@@ -0,0 +1,46 @@
+// Arithmatic Operator
+ 
+//Assign two variable
+let a = 10;
+let b = 20;
+
+//Adition
+let c = a + b;
+console.log("Addition: " + c);
+
+//Subtraction
+let d = b - a;
+console.log("Subtraction: " + d);
+
+//Multiplication
+let e = a * b;
+console.log("Multiplication: " + e);
+
+//Division
+let f = b / a;
+console.log("Division: " + f);
+
+//Modulus
+let g = b % a;
+console.log("Modulus: " + g);
+
+//Exponentiation
+let h = a ** 2;
+console.log("Exponentiation: " + h);
+
+//Increment and Decrement
+// Postfix
+let i = a++;
+console.log("Postfix Increment: " + i);
+
+// Prefix
+let j = ++a;
+console.log("Prefix Increment: " + j);
+
+// Postfix Decrement
+let k = a--;
+console.log("Postfix Decrement: " + k);
+
+// Prefix Decrement
+let l = --a;
+console.log("Prefix Decrement: " + l);
