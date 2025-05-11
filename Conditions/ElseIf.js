64e92d0696cc32b55f5d820ad5fca5bcacec7c62
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-11 11:27:18 +0530

    Add Coditionals

---
+++ Conditions/ElseIf.js
@@ -0,0 +1,8 @@
+let a = 20;
+if(a > 10) {
+    console.log('a is greater than 10');
+}else if(a > 15){
+    console.log('a is greater than 15');
+}else{
+    console.log('a is less than or equal to 10');
+}     
---
+++ Conditions/IfElse.js
@@ -0,0 +1,10 @@
+let subscription = 'super';
+if(subscription === 'super') {
+    console.log('Two Devices');
+}
+if(subscription === 'premium') {
+    console.log('Four Device');
+}
+if(subscription === 'basic') {
+    console.log('One Device');
+}
