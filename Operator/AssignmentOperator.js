5b427a54b8e4050fa097bd96eabbe92158af4335
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-11 11:08:28 +0530

    Add Operator

---
+++ Operator/AssignmentOperator.js
@@ -0,0 +1,17 @@
+let a = 20;
+let b = 10;
+
+a = a + b; // Normal Operation
+a+=b;//Short Hand Operation
+console.log(a); // 30
+
+a-= b; // Normal Operation
+console.log(a); // 20
+a*= b; // Short Hand Operation
+console.log(a); // 200
+a/= b; // Normal Operation
+console.log(a); // 20
+a%= b; // Short Hand Operation
+console.log(a); // 0
+a**= b; // Normal Operation
+console.log(a); // 1
