5e3bd30e09dc8cae0ae1046a18682a1fb2ed65e6
Hydra-Ramesh <dasramesh8343003905@gmail.com>
2025-05-11 10:33:19 +0530

    Add Logical and Relational

---
+++ Operator/LogicalOperator.js
@@ -0,0 +1,18 @@
+let  x = 10;
+let y = 20;
+let z =  10;
+
+//Logical AND (&&)
+let result = x < y && x == z;
+
+let result1 = x < y && x != z;
+
+console.log(result);
+
+//Logical OR (||)
+let result2 = x < y || x == z;
+
+let result3 = x < y || x != z;
+
+//Logical NOT (!)
+let result4 = !(x < y);
---
+++ Operator/RelationalOperator.js
@@ -0,0 +1,37 @@
+let a = 30;
+let b = 20;
+let c = '30';
+
+// Equal
+let result = a==c;
+console.log(result);
+
+// Strict Equal
+let result1 = a===c;
+console.log(result1);
+
+// Not Equal
+let result2 = a!=c;
+console.log(result2);
+
+// Strict Not Equal
+let result3 = a!==c;
+console.log(result3);
+
+// Greater Than
+let result4 = a>b;
+console.log(result4);
+
+// Greater Than or Equal
+let result5 = a>=b;
+console.log(result5);
+
+// Less Than
+let result6 = a<b;
+console.log(result6);
+
+// Less Than or Equal
+let result7 = a<=b;
+console.log(result7);
+
+
